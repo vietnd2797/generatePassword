c18b14f983ee837de1860fcca4e0cdafba027c2d
vietnd2797 <ashikaga1997@gmail.com>
2021-06-28 00:29:44 +0700

    First create

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Identity;
+
+namespace ConsoleApp1
+{
+    public class Object
+    {
+        public Object()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+        public string Id { get; set; }
+    }
+
+    class Program
+    {
+        public static string GeneratePassword(bool includeLowercase, bool includeUppercase, bool includeNumeric, bool includeSpecial, bool includeSpaces, int lengthOfPassword)
+        {
+            const int MAXIMUM_IDENTICAL_CONSECUTIVE_CHARS = 2;
+            const string LOWERCASE_CHARACTERS = "abcdefghijklmnopqrstuvwxyz";
+            const string UPPERCASE_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string NUMERIC_CHARACTERS = "0123456789";
+            const string SPECIAL_CHARACTERS = @"!#$%&*@\";
+            const string SPACE_CHARACTER = " ";
+            const int PASSWORD_LENGTH_MIN = 8;
+            const int PASSWORD_LENGTH_MAX = 128;
+
+            if (lengthOfPassword < PASSWORD_LENGTH_MIN || lengthOfPassword > PASSWORD_LENGTH_MAX)
+            {
+                return "Password length must be between 8 and 128.";
+            }
+
+            string characterSet = "";
+
+            if (includeLowercase)
+            {
+                characterSet += LOWERCASE_CHARACTERS;
+            }
+
+            if (includeUppercase)
+            {
+                characterSet += UPPERCASE_CHARACTERS;
+            }
+
+            if (includeNumeric)
+            {
+                characterSet += NUMERIC_CHARACTERS;
+            }
+
+            if (includeSpecial)
+            {
+                characterSet += SPECIAL_CHARACTERS;
+            }
+
+            if (includeSpaces)
+            {
+                characterSet += SPACE_CHARACTER;
+            }
+
+            char[] password = new char[lengthOfPassword];
+            int characterSetLength = characterSet.Length;
+
+            System.Random random = new System.Random();
+            for (int characterPosition = 0; characterPosition < lengthOfPassword; characterPosition++)
+            {
+                password[characterPosition] = characterSet[random.Next(characterSetLength - 1)];
+
+                bool moreThanTwoIdenticalInARow =
+                    characterPosition > MAXIMUM_IDENTICAL_CONSECUTIVE_CHARS
+                    && password[characterPosition] == password[characterPosition - 1]
+                    && password[characterPosition - 1] == password[characterPosition - 2];
+
+                if (moreThanTwoIdenticalInARow)
+                {
+                    characterPosition--;
+                }
+            }
+
+            return string.Join(null, password);
+        }
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 2; i++)
+            //{
+            //    var obj = new Object();
+            //    Console.WriteLine(obj.Id);            
+            //}
+            List<string> passwords = new List<string>();
+            for (int i = 0; i < 12; ++i)
+            {
+                var password = GeneratePassword(true, true, true, true, false, 12);
+                passwords.Add(password);
+            }
+            using System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@"D:\pass.txt");
+            foreach (var password in passwords)
+            {
+                file.WriteLine(password);
+            }
+        }
+    }
+}
